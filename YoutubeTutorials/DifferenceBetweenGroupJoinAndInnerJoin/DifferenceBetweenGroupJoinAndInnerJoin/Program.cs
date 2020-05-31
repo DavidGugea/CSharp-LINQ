89ab9c7c0ce0e283e2a079fc044e0c784a780a7e
David <davidgugea@yahoo.com>
2020-05-31 19:47:28 +0200

    Join Operation ( GroupJoin & InnerJoin + Differences between the two )

---
+++ YoutubeTutorials/DifferenceBetweenGroupJoinAndInnerJoin/DifferenceBetweenGroupJoinAndInnerJoin/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DifferenceBetweenGroupJoinAndInnerJoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = from e in Employee.GetAllEmployees()
+                         join d in Department.GetAllDepartments()
+                         on e.DepartmentID equals d.ID
+                         select new
+                         {
+                             e,
+                             d
+                         };
+
+            foreach(var employee in result)
+            {
+                Console.WriteLine(employee.e.Name + "\t" + employee.d.Name);
+            }
+
+            /*
+            var result = from d in Department.GetAllDepartments()
+                         join e in Employee.GetAllEmployees()
+                         on d.ID equals e.DepartmentID into eGroup
+                         select new
+                         {
+                             Department = d,
+                             Employees = eGroup
+                         };
+
+            foreach(var department in result)
+            {
+                Console.WriteLine(department.Department.Name);
+                foreach(var employee in department.Employees)
+                {
+                    Console.WriteLine(string.Format(" {0}", employee.Name));
+                }
+
+                Console.WriteLine();
+            }
+            */
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ YoutubeTutorials/InnerJoin_testTutorial/InnerJoin_testTutorial/Department.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnerJoin_testTutorial
+{
+    public class Department
+    {
+        public string name { get; set; }
+        public int ID { get; set; }
+        
+        public static List<Department> GetAllDepartments()
+        {
+            return new List<Department>()
+            {
+                new Department { name = "IT", ID = 1 },
+                new Department { name = "HR", ID = 2 },
+                new Department { name = "Payroll", ID = 3}
+            };
+        }
+    }
+}
