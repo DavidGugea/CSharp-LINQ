7f82e7978b03348468688ecd43db0f3db954a3c3
David <davidgugea@yahoo.com>
2020-05-31 14:16:39 +0200

    kudvenkat - https://www.youtube.com/watch?v=71medpGp1nc - GroupBy

---
+++ YoutubeTutorials/GroupBy_tutorial/GroupBy_tutorial/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupBy_tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            var employeeGroups = Employee.GetAllEmployees()
+                .GroupBy(x => x.Department);
+            */
+
+            var employeeGroups = from employee in Employee.GetAllEmployees()
+                                 group employee by employee.Department into eGroup
+                                 orderby eGroup.Key
+                                 select new
+                                 {
+                                     Key = eGroup.Key,
+                                     Employees = eGroup.OrderBy(employee => employee.Name)
+                                 };
+
+            foreach(var group in employeeGroups)
+            {
+                Console.WriteLine(string.Format("{0} - {1}", group.Key, group.Employees.Count()));
+                Console.WriteLine("-----------");
+                foreach(var employee in group.Employees)
+                {
+                    Console.WriteLine(employee.Name + "\t" + employee.Department);
+                }
+
+                Console.WriteLine();
+            }
+             
+            Console.ReadKey();
+        }
+    }
+}
