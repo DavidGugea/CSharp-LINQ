68759a5809267604d8af94093066347ef89cf95b
David <davidgugea@yahoo.com>
2020-06-01 15:38:37 +0200

    Cross Join & Left Outer Join

---
+++ YoutubeTutorials/CrossJoin/CrossJoin/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossJoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = Employee.GetAllEmployees().Join(
+                    Department.GetAllDepartments(),
+                    e => 2,
+                    d => 2,
+                    (e, d) => new { e, d });
+
+            foreach(var v in result)
+            {
+                Console.WriteLine(v.e.Name + "\t" + v.d.Name);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
