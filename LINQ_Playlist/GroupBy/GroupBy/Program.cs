e3225252f246d6113471890043b913429df492d7
David <davidgugea@yahoo.com>
2020-06-02 22:32:08 +0200

    GroupBy Operator

---
+++ LINQ_Playlist/GroupBy/GroupBy/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupBy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employeeGroups = from employee in Employee.GetAllEmployees()
+                                 group employee by employee.Department into eGroup
+                                 orderby eGroup.Key
+                                 select new
+                                 {
+                                     Key = eGroup.Key,
+                                     Employees = eGroup.OrderBy(x => x.Name)
+                                 };
+            
+            foreach(var group in employeeGroups)
+            {
+                Console.WriteLine("{0} - {1}", group.Key, group.Employees.Count());
+                Console.WriteLine("---------------------");
+
+                foreach(var employee in group.Employees)
+                {
+                    Console.WriteLine(employee.Name + "\t" + employee.Department);
+                }
+
+                for(int i = 0; i < 2; i++)
+                {
+                    Console.WriteLine();
+                }
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
