5d63c1684fcf7f3a153fa3e65d0090f338155c66
David <davidgugea@yahoo.com>
2020-06-02 22:58:26 +0200

    GroupBy Multiple Keys

---
+++ LINQ_Playlist/GroupBy_MultipleKeys/GroupBy_MultipleKeys/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupBy_MultipleKeys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employeeGroups = from employee in Employee.GetAllEmployees()
+                                 group employee by new { employee.Department, employee.Gender } into eGroup
+                                 orderby eGroup.Key.Department, eGroup.Key.Gender
+                                 select new
+                                 {
+                                     Dept = eGroup.Key.Department,
+                                     Gender = eGroup.Key.Gender,
+                                     Employees = eGroup.OrderBy(x => x.Name)
+                                 };
+
+            foreach(var group in employeeGroups)
+            {
+                Console.WriteLine(string.Format("{0} department {1} employees count = {2}", 
+                    group.Dept,
+                    group.Gender,
+                    group.Employees.Count()
+                ));
+                Console.WriteLine("-----------------------------------------------");
+
+                foreach(var employee in group.Employees)
+                {
+                    Console.WriteLine(employee.Name + "\t" + employee.Gender + "\t" + employee.Department);
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
