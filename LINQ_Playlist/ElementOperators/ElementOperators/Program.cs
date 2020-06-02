a1b9096b4832966717fad175785b013afd04de1e
David <davidgugea@yahoo.com>
2020-06-02 23:41:39 +0200

    Element Operators

---
+++ LINQ_Playlist/ElementOperators/ElementOperators/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElementOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3 };
+            IEnumerable<int> result = numbers.DefaultIfEmpty(10);
+            
+            foreach(int i in result)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
