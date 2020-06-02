15ada080781639cbca016e7d41e71d86202187d4
David <davidgugea@yahoo.com>
2020-06-02 22:00:40 +0200

    Cast and OfType Operators + Differences and Exercises

---
+++ LINQ_Playlist/Cast_and_OfType_Operators_AndMore/Cast_and_OfType_Operators_AndMore/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cast_and_OfType_Operators_AndMore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+            for(int i = 1; i < 4; i++)
+            {
+                list.Add(i);
+            }
+
+            list.Add("4");
+            list.Add("ABC");
+
+            IEnumerable<int> result = list.OfType<int>();
+
+            foreach(int i in result)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
