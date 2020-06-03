5baece1cedf46c2be287bfe0486a10335856c08f
David <davidgugea@yahoo.com>
2020-06-03 14:16:03 +0200

    Inner Join

---
+++ LINQ_Playlist/InnerJoin/InnerJoin/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnerJoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = from e in Employee.GetAllEmployees()
+                         join d in Department.GetAllDepartments()
+                         on e.DepartmentID equals d.ID
+                         select new
+                         {
+                             EmployeeName = e.Name,
+                             DepartmentName = d.Name
+                         };
+
+            foreach(var employee in result)
+            {
+                Console.WriteLine(employee.EmployeeName + "\t" + employee.DepartmentName);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
