4656a4c367ac559865e3c86b1c474343ea8b0cd5
David <davidgugea@yahoo.com>
2020-06-03 14:31:46 +0200

    Differences between GroupJoin & InnerJoin

---
+++ LINQ_Playlist/DifferenceBetween_GroupJoin_And_InnerJoin/DifferenceBetween_GroupJoin_And_InnerJoin/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DifferenceBetween_GroupJoin_And_InnerJoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = from e in Employee.GetAllEmployees()
+                         join d in Department.GetAllDepartments()
+                         on e.DepartmentID equals d.ID
+                         select new { e, d };
+
+            foreach(var employee in result)
+            {
+                Console.WriteLine(employee.e.Name + "\t" + employee.d.Name);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
