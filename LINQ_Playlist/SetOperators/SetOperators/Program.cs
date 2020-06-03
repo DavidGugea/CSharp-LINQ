3d63c1095dc1191e4926c775da509715dd7e7dd1
David <davidgugea@yahoo.com>
2020-06-03 19:22:49 +0200

    Set Operators

---
+++ LINQ_Playlist/SetOperators/SetOperators/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> list = new List<Employee>()
+            {
+                new Employee { ID = 101, Name = "Mike" },
+                new Employee { ID = 101, Name = "Mike" },
+                new Employee { ID = 102, Name = "Mary" }
+            };
+            var result = list.Select(x => new { x.ID, x.Name }).Distinct();
+
+            foreach(var v in result)
+            {
+                Console.WriteLine(v.ID + "\t" + v.Name);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
