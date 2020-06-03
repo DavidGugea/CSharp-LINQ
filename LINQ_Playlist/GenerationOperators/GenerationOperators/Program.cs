234108a7c773e2108931830306a33c0357855abd
David <davidgugea@yahoo.com>
2020-06-03 19:58:36 +0200

    Generation Operators

---
+++ LINQ_Playlist/GenerationOperators/GenerationOperators/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerationOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = GetData() ?? Enumerable.Empty<int>();
+            Console.WriteLine(result.Count());
+            
+                foreach (int i in result)
+                {
+                    Console.WriteLine(i);
+                }
+            
+
+            Console.ReadKey();
+        }
+
+        public static IEnumerable<int> GetData()
+        {
+            return null;
+        }
+    }
+}
