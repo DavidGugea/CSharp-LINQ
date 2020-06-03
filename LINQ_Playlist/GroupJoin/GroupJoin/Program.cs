59990f6b637630277d3e4d60a76c32d89f5aac1e
David <davidgugea@yahoo.com>
2020-06-03 14:00:41 +0200

    Group Join

---
+++ LINQ_Playlist/GroupJoin/GroupJoin/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupJoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employeesByDepartment = from d in Department.GetAllDepartments()
+                                        join e in Employee.GetAllEmployees()
+                                        on d.ID equals e.DepartmentID into eGroup
+                                        select new
+                                        {
+                                            Department = d,
+                                            Employees = eGroup
+                                        };
+
+            foreach(var department in employeesByDepartment)
+            {
+                Console.WriteLine(department.Department.Name);
+
+                foreach(var employee in department.Employees)
+                {
+                    Console.WriteLine(" " + employee.Name);
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
