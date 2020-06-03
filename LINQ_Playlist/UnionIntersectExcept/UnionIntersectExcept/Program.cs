458fdfa04c1c915a3ce8b80c11242dc2dc53cab6
David <davidgugea@yahoo.com>
2020-06-03 19:39:37 +0200

    Union & Intersect & Except

---
+++ LINQ_Playlist/UnionIntersectExcept/UnionIntersectExcept/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnionIntersectExcept
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers1 = { 1, 2, 3, 4, 5 };
+            int[] numbers2 = { 1, 3, 6, 7, 8 };
+
+            var result = numbers1.Except(numbers2);
+
+            foreach(var v in result)
+            {
+                Console.WriteLine(v);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
