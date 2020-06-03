e0b28c23fc335a7d180d6ede12cbd9e7cf5587f7
David <davidgugea@yahoo.com>
2020-06-03 19:01:12 +0200

    Cross Join

---
+++ LINQ_Playlist/CrossJoin/CrossJoin/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossJoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = Employee.GetAllEmployees()
+                .Join(Department.GetAllDepartments(),
+                e => true,
+                d => true,
+                ( e, d ) => new
+                {
+                    e, d
+                });
+
+            foreach(var v in result)
+            {
+                Console.WriteLine(v.e.Name + "\t" + v.d.Name);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
