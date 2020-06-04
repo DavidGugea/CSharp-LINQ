6087739a658b4bc1c5f22c2f38379db52f510cb0
David <davidgugea@yahoo.com>
2020-06-04 14:09:38 +0200

    Sequence Equal

---
+++ LINQ_Playlist/SequenceEqual/SequenceEqual/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SequenceEqual
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] countries1 = { "USA", "India", "UK" };
+            string[] countries2 = { "USA", "India", "Uk" };
+
+            var result = countries1.SequenceEqual(countries2, StringComparer.OrdinalIgnoreCase);
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    } 
+}
