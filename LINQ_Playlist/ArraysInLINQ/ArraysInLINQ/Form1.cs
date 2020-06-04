e9fe16eb57b9389f438e4613c14c656574b6ba6f
David <davidgugea@yahoo.com>
2020-06-04 17:36:13 +0200

    Working with Arrays in LINQ

---
+++ LINQ_Playlist/ArraysInLINQ/ArraysInLINQ/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArraysInLINQ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] a = Enumerable.Repeat(-1, 10).ToArray();
+            listBox1.DataSource = a;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int[] b = Enumerable.Range(0, 10).ToArray();
+            listBox2.DataSource = b;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int[] c = Enumerable.Range(0, 10).Select(i => 100 + 10 * i).ToArray();
+            listBox3.DataSource = c;
+        }
+    }
+}
