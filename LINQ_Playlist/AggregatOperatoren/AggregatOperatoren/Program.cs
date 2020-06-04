39aac757fe2b4111a806b47530b9d425107b61d7
David <davidgugea@yahoo.com>
2020-06-04 14:56:58 +0200

    Aggregat Operatoren

---
+++ LINQ_Playlist/AggregatOperatoren/AggregatOperatoren/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AggregatOperatoren
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> numbers = Enumerable.Range(1, 10);
+
+            foreach(int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            for(int i = 0; i < 3; i++)
+            {
+                Console.WriteLine();
+            }
+
+            Console.WriteLine(numbers.Count());
+            Console.WriteLine(numbers.Sum());
+
+            Console.ReadKey();
+        }
+    }
+}
