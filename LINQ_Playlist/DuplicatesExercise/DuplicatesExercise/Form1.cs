4436be8a2b7e1e9319db128dcbe1693123aba613
David <davidgugea@yahoo.com>
2020-06-04 17:21:53 +0200

    Duplicates Exercises

---
+++ LINQ_Playlist/DuplicatesExercise/DuplicatesExercise/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DuplicatesExercise
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<Person> personen = new List<Person>()
+        {
+            new Person ( "Müller", "Kurt" ),
+            new Person ( "Meier", "Timo" ),
+            new Person ( "Schenider", "Alois" ),
+            new Person ( "Müller", "Kurt" ),
+            new Person ( "Schröder", "Thomas" ),
+            new Person ( "Müller", "Kurt" ),
+            new Person ( "Müller", "Max" ),
+            new Person ( "Müller", "Thomas" ),
+            new Person ( "Brezner", "Dr." ),
+            new Person ( "Doberenz", "Walter" )
+        };
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IEnumerable<Person> personen1 = personen.Distinct(new PersonComparer());
+            listBox1.Items.Clear();
+            foreach(Person item in personen1)
+            {
+                listBox1.Items.Add(item);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            IEnumerable<Person> personen1 = personen.Distinct(new PersonComparer());
+
+            string[] personenNN = (from ps in personen1
+                                   select ps.Nachname).ToArray();
+            listBox1.Items.Clear();
+            foreach(string item in personenNN)
+            {
+                listBox1.Items.Add(item);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach(Person item in personen)
+            {
+                listBox1.Items.Add(item.ToString());
+            }
+        }
+    }
+}
---
+++ LINQ_Playlist/DuplicatesExercise/DuplicatesExercise/PersonComparer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuplicatesExercise
+{
+    public class PersonComparer : IEqualityComparer<Person>
+    {
+        public bool Equals(Person first, Person second)
+        {
+            return first.Vorname == second.Vorname && first.Nachname == second.Nachname;
+        }
+
+        public int GetHashCode(Person person)
+        {
+            return person.Nachname.GetHashCode() + person.Vorname.GetHashCode();
+        }
+    }
+}
