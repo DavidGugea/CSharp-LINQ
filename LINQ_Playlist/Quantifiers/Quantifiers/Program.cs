7888ada4704a6804dde35411c97998d138adbddf
David <davidgugea@yahoo.com>
2020-06-04 14:17:01 +0200

    Quantifiers

---
+++ LINQ_Playlist/Quantifiers/Quantifiers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quantifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] countries = { "USA", "INDIA", "UK" };
+            var result = countries.Contains("india", StringComparer.OrdinalIgnoreCase);
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
