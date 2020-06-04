fd18cb6d28bcc91499e79539e36e864c5d922d87
David <davidgugea@yahoo.com>
2020-06-04 15:01:04 +0200

    PLINQ

---
+++ LINQ_Playlist/PLINQ/PLINQ/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> numbers = Enumerable.Range(1, 10);
+
+            IEnumerable<int> result = numbers.AsParallel()
+                .Where(num => num % 2 == 0)
+                .OrderByDescending(x => x)
+                .Select(x => x);
+
+            foreach(int number in result)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
